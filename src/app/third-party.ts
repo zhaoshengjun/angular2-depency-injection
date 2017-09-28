75a323c48859585054c92df5dad95117272cf576
Shengjun Zhao <Shengjun.Zhao@dsiti.qld.gov.au>
2017-09-28 14:21:57 +1000

    always use OpaqueToken when using string providers

--- src/app/third-party.ts
+++ src/app/third-party.ts
@@ -1,3 +1,6 @@
+import { OpaqueToken } from "@angular/core";
+
+const API_URL = new OpaqueToken("apiUrl");
 export const THIRD_PARTY_PROVIDERS = [
-  { provide: "apiUrl", useValue: "some other value" }
+  { provide: API_URL, useValue: "some other value" }
 ];
