f0786d31e8f0a951e266b8c53dd87700852bc88b
Shengjun Zhao <Shengjun.Zhao@dsiti.qld.gov.au>
2017-09-28 12:52:15 +1000

    use data service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { DataService } from './data.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
+  providers: [DataService],
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'app';
+  items: Array<any>;
+
+  constructor(private dataService: DataService) {}
+
+  ngOnInit() {
+    this.items = this.dataService.getItems();
+  }
 }
